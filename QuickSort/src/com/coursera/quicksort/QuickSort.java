4036db5c73604343cc0c2417be9a656357b4ccf6
Shantanu Kshire <shantanu.kshire@gmail.com>
2018-09-03 18:24:05 +0530

    Quick Sort implementation

--- QuickSort/src/com/coursera/quicksort/QuickSort.java
+++ QuickSort/src/com/coursera/quicksort/QuickSort.java
@@ -2,9 +2,93 @@ package com.coursera.quicksort;
 
 public class QuickSort {
 	
+	private static volatile int comparisons = 0;
 	
-	public static void sort(int[] arr, int l, int r) {
+	public static int leftPivot(int[] arr, int l, int r) {
+		comparisons = 0;		
+		sort_leftpivot(arr,l,r);
+		return comparisons;
+	}
+	
+	public static int rightPivot(int[] arr, int l, int r) {
+		comparisons = 0;		
+		sort_rightpivot(arr,l,r);
+		return comparisons;
+	}
+	
+	public static int medianPivot(int[] arr, int l, int r) {
+		comparisons = 0;		
+		sort_medianpivot(arr,l,r);
+		return comparisons;
+	}
+	
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	private static int getMedianIdx(int arr[], int l, int m, int r) {
+		int median_idx = -1;
 		
+		double avg = (arr[l]+arr[m]+arr[r])/3.0d;
+		double diff_l = Math.abs((double)arr[l]-avg);
+		double diff_m = Math.abs((double)arr[m]-avg);
+		double diff_r = Math.abs((double)arr[r]-avg);
+		
+		if(Double.compare(diff_l, diff_r) <= 0) {
+			if(Double.compare(diff_l, diff_m) <= 0)
+				median_idx = l;
+			else 
+				median_idx = m;
+		}else {
+			if(Double.compare(diff_r, diff_m) <= 0)
+				median_idx = r;
+			else
+				median_idx = m;
+		}
+		return median_idx;
+	}
+	
+	private static int partition(int[] arr, int l, int r) {
+		int pi = arr[l];
+		int i = l;
+		for(int j=l+1; j<=r; ++j) {
+			if(arr[j] < pi) {
+				++i;
+				swap(arr,i,j);
+			}
+		}
+		comparisons += r-l;
+		swap(arr,l,i);
+		return i;
+	}
+	
+	private static void sort_leftpivot(int[] arr, int l, int r) {
+		if(l < r) {
+			int pi_idx = partition(arr,l,r);
+			sort_leftpivot(arr,l,pi_idx-1);
+			sort_leftpivot(arr,pi_idx+1,r);
+		}		
+	}
+	
+	private static void sort_rightpivot(int[] arr, int l, int r) {
+		if(l < r) {
+			swap(arr,l,r);
+			int pi_idx = partition(arr,l,r);
+			sort_rightpivot(arr,l,pi_idx-1);
+			sort_rightpivot(arr,pi_idx+1,r);
+		}		
+	}
+	
+	private static void sort_medianpivot(int[] arr, int l, int r) {
+		if(l < r) {
+			int median_idx = getMedianIdx(arr, l,(l+r)/2,r);
+			swap(arr,l,median_idx);
+			int pi_idx = partition(arr,l,r);
+			sort_medianpivot(arr,l,pi_idx-1);
+			sort_medianpivot(arr,pi_idx+1,r);
+		}		
 	}
 	
 }
--- QuickSort/src/com/coursera/quicksort/Test.java
+++ QuickSort/src/com/coursera/quicksort/Test.java
@@ -1,9 +1,55 @@
 package com.coursera.quicksort;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
+    private static final int TOTAL_SIZE = 10000;
+    private static final String FILE_PATH = "./data/QuickSort.txt";
+
+    public static void main(String args[]) throws IOException{
+
+        int result = -1;
+        int[] leftPivArr = new int[TOTAL_SIZE];
+        int[] rightPivArr = new int[TOTAL_SIZE];
+        int[] medianPivArr = new int[TOTAL_SIZE];
+
+        Path path = Paths.get(FILE_PATH);
+        Scanner scn = new Scanner(path);
+        scn.useDelimiter(System.getProperty("line.separator"));
+
+        try{
+            int i=0;
+            while(scn.hasNext()){
+            	int value = new Integer(scn.next());
+            	leftPivArr[i] = value;
+            	rightPivArr[i] = value;
+            	medianPivArr[i] = value;
+                ++i;
+            }
+
+            /* Quick Sort using first element as pivot */
+            result = QuickSort.leftPivot(leftPivArr,0,leftPivArr.length-1);
+            System.out.println("Comparisons for Left pivot: " + result);
+            
+            /*Quick Sort using last element as pivot*/
+            result = QuickSort.rightPivot(rightPivArr,0,rightPivArr.length-1);
+            System.out.println("Comparisons for Right pivot: " + result);
+            
+            /*Quick Sort using median as pivot*/
+            result = QuickSort.medianPivot(medianPivArr,0,medianPivArr.length-1);
+            System.out.println("Comparisons for Median pivot: " + result);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }finally {
+            scn.close();
+        }
+
+    }
 
 }
